242c72c745520e5ca5c18901804772ac2e8c37ce
patrickCNMartin <pcnmartin@gmail.com>
2025-11-19 16:32:46 -0800

    nix shell update - added rix and devtools - purged rprofile

---
+++ rix.R
@@ -0,0 +1,32 @@
+library(rix)
+
+rix(
+  r_ver = "latest-upstream",
+  r_pkgs = c(
+  "ggplot2",
+  "patchwork",
+  "ggpubr",
+  "rjson",
+  "Matrix",
+  "hdf5r",
+  "Seurat",
+  "RColorBrewer",
+  "knitr",
+  "RUnit",
+  "rmarkdown",
+  "testthat",
+  "roxygen2",
+  "devtools",
+  "rix",
+  "rlang"),
+  system_pkgs = NULL,
+  git_pkgs = list(
+    package_name = "Vesalius",
+    repo_url = "https://github.com/WonLab-CS/Vesalius",
+    commit = "bb4aa3d"
+  ),
+  ide = "none",
+  project_path = ".",
+  overwrite = TRUE,
+  print = TRUE
+)
