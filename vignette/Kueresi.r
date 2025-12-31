4a805db100b9ed1d5a49dce8e9d8c88f8be35425
patrickCNMartin <pcnmartin@gmail.com>
2025-12-31 13:31:55 -0800

    flake update:

---
+++ vignette/Kueresi.r
@@ -0,0 +1,17 @@
+---
+title: "Kuresi Vignette"
+author: "Patrick C.N. Martin"
+date: "`r Sys.Date()`"
+output: 
+    pdf_document
+    html_document
+---
+
+
+```{r setup, eval = TRUE, echo = FALSE}
+knitr::opts_chunk$set(warning = FALSE, message = FALSE)
+
+```
+```{r libs, eval = TRUE, echo  = FALSE, message = FALSE, warning = FALSE}
+library(Kuresi)
+```
